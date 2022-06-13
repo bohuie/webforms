a493c0af965f50248a2b7434797f3f8690ad10a5
stejay91 <stejay91@hotmail.com>
2022-06-13 01:29:17 -0700

    Finshed managing staff accounts.

--- webforms/lib/editstaffaccount.dart
+++ webforms/lib/editstaffaccount.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:webforms/ManageAccount.dart';
 import 'loggedinhome.dart';
 
-class staffbutton extends StatelessWidget {
-  const staffbutton({super.key});
+class editstaffbutton extends StatelessWidget {
+  const editstaffbutton({super.key});
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -21,7 +21,6 @@ class staffbutton extends StatelessWidget {
 
   void navigate(BuildContext context) {
     Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => LoggedInPage()));
+        MaterialPageRoute(builder: (context) => ManageAccounts()));
   }
 }
-
